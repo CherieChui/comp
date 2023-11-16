58ab7b93e7fd237215f5ac9d19945866b9c1d433
AK476666 <97866338+AK476666@users.noreply.github.com>
2023-11-16 20:29:38 +0800

    Update US2.py

--- US2.py
+++ US2.py
@@ -1,23 +1,60 @@
 from builtins import input
 import os
+from datetime import datetime
+import json
 
-def create_pir():
-    file_name = input("Enter the file name for the new PIR: ")
+class Task:
+    def __init__(self, name, description, deadline):
+        if not isinstance(name, str) or not isinstance(description, str):
+            raise ValueError("Name and Description must be of type string.")
+        self.name = name
+        self.description = description
+        self.deadline = deadline
+        self.task_dict = {"name": self.name, "description": self.description, "deadline": self.deadline}
 
-    if os.path.exists(file_name):
-        print(file_name + " file already exists.")
-    else:
-        try:
-            with open(file_name, 'w') as file:
-                print("New PIR created: " + file_name)
-            with open(file_name, 'r') as file:
-                pir_text = file.read()
-                if pir_text:
-                    print("PIR content:")
-                    print(pir_text)
-                else:
-                    print("Empty PIR file.")
-        except IOError:
-            print("Error: Unable to create or open the PIR file.")
+class TaskManager:
+    def __init__(self, file_name):
+        self.tasks = []
+        self.file_name = file_name
+        if not os.path.exists(self.file_name):
+            print("The file doesn't exist")
+        
+    def add_task(self, name, description, deadline):
+        task = Task(name, description, deadline)
+        self.tasks.append(task)
+        if os.path.exists(self.file_name):
+            self.write_task_to_file(task)
+            print("Task added successfully!")
+        
+    def display_tasks(self):
+        if self.tasks:
+            for task in self.tasks:
+                print(f"Task name: {task.task_dict['name']}\nDescription: {task.task_dict['description']}\nDeadline: {task.task_dict['deadline']}\n-----------------------")
+                
+    def write_task_to_file(self, task):
+        with open(self.file_name, 'a') as file:
+            file.write(json.dumps(task.task_dict))
+            file.write("\n")
 
-create_pir()
+def validate_date(deadline_string): 
+    try:
+        datetime.strptime(deadline_string, "%Y-%m-%d")
+        return True
+    except ValueError:
+        return False 
+
+file_name = input("Enter the existing file name: ")
+task_manager = TaskManager(file_name)
+
+if os.path.exists(file_name):
+    task_name = input("Enter the task name: ")
+    task_description = input("Enter the task description: ")
+    task_deadline = input("Enter the deadline of task in form of YYYY-MM-DD: ")
+    while not validate_date(task_deadline):
+        print("The input deadline is incorrect!")
+        task_deadline = input("Enter the deadline of task in form of YYYY-MM-DD: ")
+
+    task_manager.add_task(task_name, task_description, task_deadline)
+
+    print("\nTask Added Content:")
+    task_manager.display_tasks()
