a58a2baa418192d03851ae445432b9fdd604e626
Jeremy <112054589+MioSena@users.noreply.github.com>
2023-11-24 13:16:56 +0800

    Update controller_test.py

--- controller_test.py
+++ controller_test.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import patch
-from controller import check_whether_file_exist, check_pim_files, print_all_files
+from controller import check_whether_file_exist, check_pim_files, print_all_files, main
 
 class TestController(unittest.TestCase):
 
@@ -17,7 +17,8 @@ class TestController(unittest.TestCase):
     @patch('os.listdir')
     def test_print_all_files(self, mock_listdir):
         mock_listdir.return_value = ['test.pim']
-        self.assertTrue(print_all_files())  
+        self.assertTrue(print_all_files())
+
 
 if __name__ == '__main__':
     unittest.main()
